d857475bd94765253f55af463890de111586d733
Bgrerr <destructionlab@gmail.com>
2025-04-30 17:02:29 -0500

    Update reservationController.js

--- server/controllers/reservationController.js
+++ server/controllers/reservationController.js
@@ -1,22 +1,43 @@
 const Reservation = require('../models/Reservation');
 const ReservationFactory = require('../factories/ReservationFactory');
 
+// Crear reserva
 exports.createReservation = async (req, res) => {
   try {
+    const { name, email, date, time, people } = req.body;
+
+    if (!name || !email || !date || !time || !people) {
+      return res.status(400).json({ message: 'Todos los campos son obligatorios' });
+    }
+
     const reservation = ReservationFactory.create(req.body);
     await reservation.save();
+
     res.status(201).json(reservation);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 };
 
+// Obtener todas las reservas
 exports.getReservations = async (req, res) => {
-  const reservations = await Reservation.find();
-  res.json(reservations);
+  try {
+    const reservations = await Reservation.find();
+    res.json(reservations);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
+// Eliminar reserva por ID
 exports.deleteReservation = async (req, res) => {
-  await Reservation.findByIdAndDelete(req.params.id);
-  res.json({ message: 'Reserva eliminada' });
+  try {
+    const deleted = await Reservation.findByIdAndDelete(req.params.id);
+    if (!deleted) {
+      return res.status(404).json({ message: 'Reserva no encontrada' });
+    }
+    res.json({ message: 'Reserva eliminada' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
