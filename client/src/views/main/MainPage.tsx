977e47ee1523707ced033186235e25f0426a6d49
Bgrerr <destructionlab@gmail.com>
2025-04-30 18:35:47 -0500

    Update MainPage.tsx

--- client/src/views/main/MainPage.tsx
+++ client/src/views/main/MainPage.tsx
@@ -1,18 +1,22 @@
-// src/views/MainPage.tsx
+import React from 'react';
 import { Link } from 'react-router-dom';
-export default function MainPage() {
-    return (
-        <div style={{ padding: '2rem' }}>
-          <h1>App reservas restaurante</h1>
-          <ul style={{ listStyle: 'none', lineHeight: '2rem' }}>
-            <li><Link to="/login">Login Cliente</Link></li>
-            <li><Link to="/register">Registro Cliente</Link></li>
-            <li><Link to="/admin/login">Login Administrador</Link></li>
-            <li><Link to="/admin/register">Registro Administrador</Link></li>
-            <li><Link to="/client/dashboard">Dashboard Cliente</Link></li>
-            <li><Link to="/admin/dashboard">Dashboard Administrador</Link></li>
-          </ul>
-        </div>
-      );
-  }
+
+const MainPage: React.FC = () => {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Bienvenido a ZitApp</h1>
+      <nav>
+        <ul>
+          <li><Link to="/clientlogin">Login Cliente</Link></li>
+          <li><Link to="/clientregister">Registro Cliente</Link></li>
+          <li><Link to="/adminlogin">Login Admin</Link></li>
+          <li><Link to="/adminregister">Registro Admin</Link></li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default MainPage;
+
   
